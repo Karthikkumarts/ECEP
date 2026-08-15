492483c29eb14f96eb2ae9fb88c4b623f40c10ba
karthik <karthikkumarts1999@gmail.com>
2026-08-15 12:58:47 +0530

    added semun union

--- LI/classwork/system_calls/mutex_and_semaphore/process_semaphores.c
+++ LI/classwork/system_calls/mutex_and_semaphore/process_semaphores.c
@@ -4,7 +4,10 @@
 #include<sys/sem.h>
 #include<sys/ipc.h>
 #include<sys/wait.h>
-
+union semun
+{
+	int val;
+};
 int main()
 {
     pid_t pid;
@@ -12,7 +15,9 @@ int main()
     struct sembuf op;
     key_t key = 'A';
     semid = semget(key,1,IPC_CREAT|0666);
-    semctl(semid,1,SETVAL,1);
+    union semun ap;
+    ap.val =1;
+    semctl(semid,0,SETVAL,ap);
 
     pid = fork(); 
 
@@ -29,7 +34,7 @@ int main()
         semop(semid,&op,1);
 
 	printf("child is in critical section\n");
-	sleep(10);
+	sleep(1);
 	printf("child is coming out of  critical section\n");
 
 	op.sem_num = 0;
@@ -40,20 +45,21 @@ int main()
 	break;
     default:
 	printf("--------------parent process------------\n");
-//	wait(NULL);
 	op.sem_num = 0;
 	op.sem_op = -1;
 	op.sem_flg = 0;
         semop(semid,&op,1);
 
 	printf("parent is in critical section\n");
-	sleep(10);
+	sleep(1);
 	printf("parent is coming out of  critical section\n");
 
 	op.sem_num = 0;
 	op.sem_op = 1;
 	op.sem_flg = 0;
         semop(semid,&op,1);
+	wait(NULL);
+	printf("back to parent after wait\n");
 	break;
     }
 }
