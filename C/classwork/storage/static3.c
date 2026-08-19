ae93d76d8d4fa2fede138bc5f9d335b386884bfc
Karthikkumarts <137600362+Karthikkumarts@users.noreply.github.com>
2026-08-19 18:41:31 +0530

    Update static3.c

--- C/classwork/storage/static3.c
+++ C/classwork/storage/static3.c
@@ -4,6 +4,7 @@ int *foo()
 
 int i=10;
 static int *j; //*j=&i doesnt work
+// Because i is a local stack variable, its memory address (&i) is dynamic and only decided at runtime when the function is called. The compiler cannot resolve this address during compilation, resulting in a compile-time error.
 j=&i;
 return j;
 }
